71d120b4f491984c9c85e63d713f5e5fbc06d110
githubscientist <sathish.datascientist@gmail.com>
2025-02-22 11:57:50 +0530

    redux setup done --register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,12 +1,39 @@
+import { useDispatch, useSelector } from "react-redux";
+import { selectEmail, selectName, selectPassword, setEmail, setName, setPassword } from "../redux/features/auth/registerSlice";
+
 const Register = () => {
+
+    const name = useSelector(selectName);
+    const email = useSelector(selectEmail);
+    const password = useSelector(selectPassword);
+
+    const dispatch = useDispatch();
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+
+        console.log(name, email, password);
+    }
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
             <div className="w-full max-w-md p-6 space-y-4 bg-white shadow-lg rounded-2xl">
                 <h2 className="text-2xl font-bold text-center text-gray-800">Register</h2>
-                <form className="space-y-4">
-                    <input type="text" placeholder="Name..." className="w-full p-3 border border-gray-300 rounded-lg" />
-                    <input type="email" placeholder="Email..." className="w-full p-3 border border-gray-300 rounded-lg" />
-                    <input type="password" placeholder="Password..." className="w-full p-3 border border-gray-300 rounded-lg" />
+                <form className="space-y-4"
+                    onSubmit={handleRegister}
+                >
+                    <input type="text" placeholder="Name..." className="w-full p-3 border border-gray-300 rounded-lg"
+                        value={name}
+                        onChange={(e) => dispatch(setName(e.target.value))}
+                    />
+                    <input type="email" placeholder="Email..." className="w-full p-3 border border-gray-300 rounded-lg"
+                        value={email}
+                        onChange={(e) => dispatch(setEmail(e.target.value))}
+                    />
+                    <input type="password" placeholder="Password..." className="w-full p-3 border border-gray-300 rounded-lg"
+                        value={password}
+                        onChange={(e) => dispatch(setPassword(e.target.value))}
+                    />
                     <button type="submit" className="w-full p-3 bg-blue-500 text-white rounded-lg">Register</button>
                 </form>
             </div>
