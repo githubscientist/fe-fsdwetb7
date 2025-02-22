58c491948bd04277ba5593e99e30f9ae581861f9
githubscientist <sathish.datascientist@gmail.com>
2025-02-22 11:13:47 +0530

    react router upgrade and setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router-dom";
+
+
+const routes = [
+  {
+    path: "/",
+    element: <h1>Hello World!</h1>
+  }
+];
+
+const router = createBrowserRouter(routes, {
+  future: {
+    v7_relativeSplatPath: true,
+    v7_partialHydration: true,
+    v7_skipActionErrorRevalidation: true,
+    v7_normalizeFormMethod: true,
+    v7_fetcherPersist: true,
+  }
+});
+
 const App = () => {
-  return (
-    <div>App</div>
-  )
+
+  return <RouterProvider
+    router={router}
+    future={{
+      v7_startTransition: true,
+    }}
+  />;
 }
 
 export default App;
